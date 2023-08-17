90dff53455c9bb15315050c03ddef1c7bc2a7dbf
Cel Pynenborg <cel.pynenborg@telenetgroup.be>
2023-08-17 15:56:33 +0200

    Create integration testing module

---
+++ integration-testing/src/main/java/com/axxes/integrationtesting/IntegrationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.axxes.integrationtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationTestingApplication.class, args);
+	}
+
+}
---
+++ integration-testing/src/test/java/com/axxes/integrationtesting/IntegrationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axxes.integrationtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
